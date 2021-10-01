79539f5279ba1721d4b65e6dfacd2514bbe30e32
Raju9143 <rahul9143roy@gmail.com>
2021-10-01 18:59:53 +0530

    ...

--- my_tree_garden/admin/views.py
+++ my_tree_garden/admin/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<b>gandu</b>")
+    return HttpResponse("<b>It is admin index page</b>")
