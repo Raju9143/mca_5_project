c9b48fd37e38f4081208e63ef7ab201fd74634b0
Raju9143 <rahul9143roy@gmail.com>
2021-10-04 11:11:55 +0530

    up_p

--- my_tree_garden/my_tree_garden/urls.py
+++ my_tree_garden/my_tree_garden/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('dadmin/', admin.site.urls),
-    path('admin/',include('admin.urls')),
+    path('admin/', admin.site.urls),
+    path('tadmin/',include('tadmin.urls')),
     path('',include('user.urls')),
 ]
